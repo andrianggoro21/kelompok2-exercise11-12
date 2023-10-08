e749b9badeb278433fa358a5241167a3effa4862
Rizky Noor Ichwanudin <rizky.noor.ichwanudin@gmail.com>
2023-10-08 20:38:57 +0700

    Update-Register

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom"
 import Register from "./pages/register";
+import Login from "./pages/login";
 import DisplayData from "./pages/displayData";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Register/>}></Route>
-      <Route path="/DisplayData" element={<DisplayData/>}></Route>
+      <Route path="/login" element={<Login/>}></Route>
+      <Route path="/display-data" element={<DisplayData/>}></Route>
     </Routes>
   );
 }
