f485f4a0b11f1bea31502c654b5c4d0a80709de0
Ryan Praka Adikawa <142961699+rypdw@users.noreply.github.com>
2023-10-09 13:23:50 +0700

    edit

--- src/components/boxLogin/index.jsx
+++ src/components/boxLogin/index.jsx
@@ -1,72 +1,166 @@
-import { useState } from "react";
+import { useState, useEffect } from 'react';
+import {
+  Flex,
+  Box,
+  Heading,
+  Button,
+  Avatar,
+  Input,
+  FormControl,
+  FormLabel,
+  InputGroup,
+  InputRightElement,
+  Text,
+} from '@chakra-ui/react';
+import { Formik, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import axios from 'axios';
+
+const LoginSchema = Yup.object().shape({
+  email: Yup.string()
+  .email('Invalid email address format')
+  .required('Email is required'),
+  password: Yup.string()
+  .required('Password is required'),
+});
 
 const BoxLogin = () => {
-    const [formData, setFormData] = useState({ username: '', password: '' });
   const [authenticated, setAuthenticated] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [users, setUsers] = useState([]);
 
-  const validUsers = [
-    { username: 'user1', password: 'password1' },
-    { username: 'user2', password: 'password2' },
-  ];
+  useEffect(() => {
+    axios.get(" http://localhost:3000/users")
+      .then((response) => {
+        setUsers(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+    
+    const handleLogin = (values) => {
+    const { username, password } = values;
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
-
-  const handleLogin = () => {
-    const { username, password } = formData;
-
-    const isValidUser = validUsers.some(
+    const isValidUser = users.some(
       (user) => user.username === username && user.password === password
     );
 
     if (isValidUser) {
-      // Autentikasi berhasil
+
       setAuthenticated(true);
-      alert('Autentikasi berhasil!');
+      alert('Login successful!');
     } else {
       // Autentikasi gagal
-      alert('Autentikasi gagal. Periksa kembali username dan password Anda.');
+      alert('Login failed. Please check your username and password.');
     }
   };
 
   const handleLogout = () => {
     // Logout pengguna
     setAuthenticated(false);
-    setFormData({ username: '', password: '' });
   };
 
   return (
-    <div>
-      <h1>Aplikasi Autentikasi</h1>
-      {authenticated ? (
-        <div>
-          <p>Selamat datang, {formData.username}!</p>
-          <button onClick={handleLogout}>Logout</button>
-        </div>
-      ) : (
-        <div>
-          <input
-            type="text"
-            placeholder="Username"
-            name="username"
-            value={formData.username}
-            onChange={handleInputChange}
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            name="password"
-            value={formData.password}
-            onChange={handleInputChange}
-          />
-          <button onClick={handleLogin}>Login</button>
-        </div>
-      )}
-    </div>
+    <Flex
+      height="100vh"
+      alignItems="center"
+      justifyContent="center"
+      bg="twitter.100"
+    >
+      <Box
+        bg="white"
+        p={8}
+        borderRadius="md"
+        boxShadow="md"
+        maxWidth="400px"
+        width="100%"
+      >
+        {authenticated ? (
+          <Flex flexDirection="column" alignItems="center">
+            <Avatar
+              size="xl"
+              name="User"
+              src="https://via.placeholder.com/150"
+              mb={4}
+            />
+            <Text fontSize="xl" fontWeight="bold" mb={2}>
+              Welcome 😊, User!
+            </Text>
+            <Button
+              onClick={handleLogout}
+              colorScheme="twitter"
+              mt={4}
+              size="md"
+            >
+              Logout
+            </Button>
+          </Flex>
+        ) : (
+          <div>
+            <Heading mb={4} size="lg" fontWeight="bold">
+              Login
+            </Heading>
+            <Formik
+              initialValues={{ username: '', password: '' }}
+              validationSchema={LoginSchema}
+              onSubmit={(values) => handleLogin(values)}
+            >
+              {({ values, handleChange }) => (
+                <Form>
+                  <FormControl>
+                    <FormLabel>Username or Email</FormLabel>
+                    <Input
+                      type="text"
+                      name="username"
+                      value={values.username}
+                      onChange={handleChange}
+                    />
+                    <ErrorMessage name="username" component="div" className="error-message" style={{ color: "red" }}/>
+                  </FormControl>
+
+                  <FormControl mt={4}>
+                    <FormLabel>Password</FormLabel>
+                    <InputGroup>
+                      <Input
+                        type={showPassword ? 'text' : 'password'}
+                        name="password"
+                        value={values.password}
+                        onChange={handleChange}
+                      />
+                      <InputRightElement width="4.5rem">
+                        <Button
+                          h="1.75rem"
+                          size="sm"
+                          onClick={() => setShowPassword(!showPassword)}
+                        >
+                          {showPassword ? <FaEyeSlash /> : <FaEye />}
+                        </Button>
+                      </InputRightElement>
+                    </InputGroup>
+                    <ErrorMessage name="password" component="div" className="error-message" />
+                  </FormControl>
+
+                  <Button
+                    type="submit"
+                    colorScheme="twitter"
+                    bg="twitter.400"
+                    mt={4}
+                    size="lg"
+                    w="100%"
+                  >
+                    Login
+                  </Button>
+                </Form>
+              )}
+            </Formik>
+          </div>
+        )}
+      </Box>
+    </Flex>
   );
-     
 };
 
 export default BoxLogin;
