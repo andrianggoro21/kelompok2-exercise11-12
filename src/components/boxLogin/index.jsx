dc0e73b6cc347de9ce8d74d0cf78cf4057ab1c52
Ryan Praka Adikawa <142961699+rypdw@users.noreply.github.com>
2023-10-09 22:18:23 +0700

    Ryan (#5)
    * Dev (#3)
    
    * First Commit
    
    * Update-Register (#1)
    
    * Update Login
    
    ---------
    
    Co-authored-by: Rizky Noor Ichwanudin <rizky.noor.ichwanudin@gmail.com>
    Co-authored-by: Rizky Noor Ichwanudin <46215481+kinur31@users.noreply.github.com>
    
    * edit
    
    * create
    
    ---------
    
    Co-authored-by: Rizky Noor Ichwanudin <rizky.noor.ichwanudin@gmail.com>
    Co-authored-by: Rizky Noor Ichwanudin <46215481+kinur31@users.noreply.github.com>

--- src/components/boxLogin/index.jsx
+++ src/components/boxLogin/index.jsx
@@ -1,72 +1,215 @@
-import { useState } from "react";
+import { useState, useEffect } from 'react';
+import {
+  Flex,
+  Box,
+  Heading,
+  Button,
+  Avatar,
+  Input,
+  FormControl,
+  FormLabel,
+  InputGroup,
+  InputRightElement,
+  Text,
+} from '@chakra-ui/react';
+import { Formik, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import axios from 'axios';
+import { useNavigate, Link } from 'react-router-dom';
+import { useFormik } from "formik";
+
+const LoginSchema = Yup.object().shape({
+  email: Yup.string()
+  .email('Invalid email address format')
+  .required('Email is required'),
+  password: Yup.string()
+  .required('Password is required'),
+});
 
 const BoxLogin = () => {
-    const [formData, setFormData] = useState({ username: '', password: '' });
-  const [authenticated, setAuthenticated] = useState(false);
+  const [users, setUsers] = useState([]);
+  const Navigate = useNavigate();
+ const [showPassword, setShowPassword] = useState(false);
 
-  const validUsers = [
-    { username: 'user1', password: 'password1' },
-    { username: 'user2', password: 'password2' },
-  ];
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
-
-  const handleLogin = () => {
-    const { username, password } = formData;
-
-    const isValidUser = validUsers.some(
-      (user) => user.username === username && user.password === password
-    );
-
-    if (isValidUser) {
-      // Autentikasi berhasil
-      setAuthenticated(true);
-      alert('Autentikasi berhasil!');
-    } else {
-      // Autentikasi gagal
-      alert('Autentikasi gagal. Periksa kembali username dan password Anda.');
+  const fatchDataLogin = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/users");
+      setUsers(response.data);
+    } catch (err) {
+      console.log(err);
     }
   };
 
-  const handleLogout = () => {
-    // Logout pengguna
-    setAuthenticated(false);
-    setFormData({ username: '', password: '' });
+  const allEmail = users.map((item) => item.email);
+  const allPassword = users.map((item) => item.password);
+  useEffect(() => {
+    fatchDataLogin();
+  }, []);
+
+    
+  const check = (email, password) => {
+    if (allEmail.includes(email) && allPassword.includes(password)) {
+      alert('succes')
+   
+     
+    } else if (
+      allEmail.includes(email) &&
+      !allPassword.includes(password)
+    ) {
+      alert("Password salah");
+    } else {
+      alert("Email Belum Terdaftar");
+    }
   };
 
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: LoginSchema,
+    onSubmit: (values) => {
+      check(values.email, values.password);
+      Navigate("/register");
+    },
+  });
+
   return (
-    <div>
-      <h1>Aplikasi Autentikasi</h1>
-      {authenticated ? (
-        <div>
-          <p>Selamat datang, {formData.username}!</p>
-          <button onClick={handleLogout}>Logout</button>
-        </div>
-      ) : (
-        <div>
-          <input
-            type="text"
-            placeholder="Username"
-            name="username"
-            value={formData.username}
-            onChange={handleInputChange}
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            name="password"
-            value={formData.password}
-            onChange={handleInputChange}
-          />
-          <button onClick={handleLogin}>Login</button>
-        </div>
-      )}
-    </div>
+    <Flex
+      height="100vh"
+      alignItems="center"
+      justifyContent="center"
+      bg="twitter.100"
+    >
+      <Box
+        bg="white"
+        p={8}
+        borderRadius="md"
+        boxShadow="md"
+        maxWidth="400px"
+        width="100%"
+      >
+        {/* {authenticated ? ( */}
+          {/* <Flex flexDirection="column" alignItems="center">
+            <Avatar
+              size="xl"
+              name="User"
+              src="https://via.placeholder.com/150"
+              mb={4}
+            />
+            <Text fontSize="xl" fontWeight="bold" mb={2}>
+              Welcome 😊, User!
+            </Text>
+            <Button
+              onClick={handleLogout}
+              colorScheme="twitter"
+              mt={4}
+              size="md"
+            >
+              Logout
+            </Button>
+          </Flex>
+        ) : ( */}
+          <div>
+            <Heading mb={4} size="lg" fontWeight="bold">
+              Login
+            </Heading>
+            <form onSubmit={formik.handleSubmit}>
+
+                  <FormControl>
+                    <FormLabel>Email</FormLabel>
+                    <Input
+                      type="text"
+                      name="email"
+                      value={formik.values.email}
+                      onChange={formik.handleChange}
+                    />
+                    {/* <ErrorMessage name="username" component="div" className="error-message" style={{ color: "red" }}/> */}
+                  </FormControl>
+
+                  <FormControl mt={4}>
+                    <FormLabel>Password</FormLabel>
+                    <InputGroup>
+                      <Input
+                        type={showPassword ? 'text' : 'password'}
+                        name="password"
+                        value={formik.values.password}
+                        onChange={formik.handleChange}
+                      />
+                      <InputRightElement width="4.5rem">
+                        <Button
+                          h="1.75rem"
+                          size="sm"
+                          onClick={() => setShowPassword(!showPassword)}
+                        >
+                          {showPassword ? <FaEyeSlash /> : <FaEye />}
+                        </Button>
+                      </InputRightElement>
+                    </InputGroup>
+                    {/* <ErrorMessage name="password" component="div" className="error-message" /> */}
+                  </FormControl>
+
+                  <Button
+                    type="submit"
+                    colorScheme="twitter"
+                    bg="twitter.400"
+                    mt={4}
+                    size="lg"
+                    w="100%"
+                  >
+                    Login
+                  </Button>
+                </form>
+          </div>
+      </Box>
+    </Flex>
+    // <Box>
+		// 	<Box
+		// 		bgColor="white"
+		// 		p="50px"
+		// 		borderRadius="10px"
+		// 		color="black"
+    //             w="200px"
+		// 	>
+		// 		<form onSubmit={formik.handleSubmit}>
+		// 			<Text fontSize="20px">Login</Text>
+		// 			<FormControl display="flex" flexDirection="column" justifyContent="start">
+		// 				<FormLabel>Email</FormLabel>
+		// 				<Input
+		// 					type="text"
+    //                         name="email"
+		// 					value={formik.values.email}
+		// 					onChange={formik.handleChange}
+    //                         borderRadius="5px"
+    //                         bgColor="transparent"
+    //                         border="1px solid black"
+    //                         h="25px"
+    //                         color="black"
+		// 				/>
+		// 			</FormControl>
+		// 			<FormControl display="flex" flexDirection="column" justifyContent="start">
+		// 				<FormLabel>Password</FormLabel>
+		// 				<Input
+		// 					type="password"
+    //                         name="password"
+		// 					value={formik.values.password}
+		// 					onChange={formik.handleChange}
+    //                         borderRadius="5px"
+    //                         bgColor="transparent"
+    //                         border="1px solid black"
+    //                         h="25px"
+    //                         color="black"
+		// 				/>
+		// 			</FormControl>
+		// 			<Button type="submit" mt="30px" p="6px 20px" textAlign="center" fontSize="16px">Login</Button>
+		// 		</form>
+		// 		<Box>
+		// 			<Text>Belum punya akun <Link to="/register">Register</Link></Text>
+		// 		</Box>
+		// 	</Box>
+		// </Box>
   );
-     
 };
 
 export default BoxLogin;
