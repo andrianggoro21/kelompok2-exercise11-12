e349de87120e2f4ebade04acef76810723c260c2
Rizky Noor Ichwanudin <rizky.noor.ichwanudin@gmail.com>
2023-10-09 23:38:02 +0700

    Update Register

--- src/components/boxRegister/index.jsx
+++ src/components/boxRegister/index.jsx
@@ -11,6 +11,7 @@ import {
   FormHelperText,
   InputGroup,
   Input,
+  InputRightElement,
   Button,
   FormErrorMessage,
   Stack,
@@ -20,6 +21,8 @@ import axios from "axios";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const RegisterSchema = Yup.object().shape({
   name: Yup.string()
@@ -67,6 +70,8 @@ const BoxRegister = () => {
       navigate("/login");
     },
   });
+
+  const [showPassword, setShowPassword] = useState(false);
 
   return (
     <ChakraProvider theme={formFloating}>
@@ -118,11 +123,20 @@ const BoxRegister = () => {
                 <InputGroup>
                   <Input
                     placeholder=" "
-                    type="password"
+                    type={showPassword ? "text" : "password"}
                     name="password"
                     values={formik.values.password}
                     onChange={formik.handleChange}
                   ></Input>
+                  <InputRightElement>
+                    <Button
+                      h="100%"
+                      size="sm"
+                      onClick={() => setShowPassword(!showPassword)}
+                    >
+                      {showPassword ? <FaEyeSlash /> : <FaEye />}
+                    </Button>
+                  </InputRightElement>
                   <FormLabel>Password</FormLabel>
                 </InputGroup>
                 {formik.touched.password && formik.errors.password && (
