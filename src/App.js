415fa3620dd0a8b72725fb87610a6519015dbb19
Rizky Noor Ichwanudin <rizky.noor.ichwanudin@gmail.com>
2023-10-11 20:43:44 +0700

    Add PrivateRoutes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { Routes, Route } from "react-router-dom"
-import Home from "./pages/home";
+import { Routes, Route } from "react-router-dom";
+import PrivateRoutes from "./utils/PrivateRoutes";
 import Register from "./pages/register";
 import Login from "./pages/login";
 import DisplayData from "./pages/displayData";
@@ -7,10 +7,19 @@ import DisplayData from "./pages/displayData";
 function App() {
   return (
     <Routes>
+<<<<<<< Updated upstream
       <Route path="/" element={<Home/>}></Route>
       <Route path="/register" element={<Register/>}></Route>
       <Route path="/login" element={<Login/>}></Route>
       <Route path="/display-data" element={<DisplayData/>}></Route>
+=======
+      <Route element={<PrivateRoutes />}>
+        <Route element={<Timeline />} path="/" exact />
+        <Route element={<DisplayData />} path="/display-data" />
+      </Route>
+      <Route element={<Register />} path="/register" />
+      <Route element={<Login />} path="/login" />
+>>>>>>> Stashed changes
     </Routes>
   );
 }
